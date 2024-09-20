a55881342d144dfcf50ce63861d7c865512d34c4
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-09-20 15:25:55 +0500

    Add parameters and lang files

---
+++ template/lang/en/.parameters.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['T_IBLOCK_DESC_NEWS_NAME'] = "Display news title";
+$MESS ['T_IBLOCK_DESC_NEWS_PICTURE'] = "Display preview background picture";
+$MESS ['T_IBLOCK_DESC_NEWS_TEXT'] = "Display element preview text";
+?>
---
+++ template/lang/ru/.parameters.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['T_IBLOCK_DESC_NEWS_NAME'] = "Выводить название новости";
+$MESS ['T_IBLOCK_DESC_NEWS_PICTURE'] = "Выводить фоновое изображение анонса";
+$MESS ['T_IBLOCK_DESC_NEWS_TEXT'] = "Выводить текст анонса";
+?>
