867184e25ac74c563f51f50a199ce6870b9b1897
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-09-27 18:58:02 +0500

    Update template

--- template/template.php
+++ template/template.php
@@ -28,7 +28,8 @@ $ans_array = $arResult["arAnswers"];
             ?>
                 <div class="input contact-form__input"><label class="input__label" for="medicine_name">
                     <div class="input__label-text">Ваше имя*</div>
-                    <input class="input__input" type="text" id="medicine_name" name="<?=$inp_name?>" value="" required="">
+                    <input class="input__input" type="text" id="medicine_name" name="<?=$inp_name?>" value=""
+                    required="">
                     <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
                 </label></div>
             <?php } ?>
@@ -41,7 +42,8 @@ $ans_array = $arResult["arAnswers"];
 			?>
                 <div class="input contact-form__input"><label class="input__label" for="medicine_company">
                     <div class="input__label-text">Компания/Должность*</div>
-                    <input class="input__input" type="text" id="medicine_company" name="<?=$inp_name?>" value="" required="">
+                    <input class="input__input" type="text" id="medicine_company" name="<?=$inp_name?>" value=""
+                    required="">
                     <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
                 </label></div>
             <?php } ?>
@@ -54,7 +56,8 @@ $ans_array = $arResult["arAnswers"];
 			?>
                 <div class="input contact-form__input"><label class="input__label" for="medicine_email">
                     <div class="input__label-text">Email*</div>
-                    <input class="input__input" type="email" id="medicine_email" name="<?=$inp_name?>" value="" required="">
+                    <input class="input__input" type="email" id="medicine_email" name="<?=$inp_name?>" value=""
+                    required="">
                     <div class="input__notification">Неверный формат почты</div>
                 </label></div>
             <?php } ?>
@@ -67,7 +70,9 @@ $ans_array = $arResult["arAnswers"];
 			?>
                 <div class="input contact-form__input"><label class="input__label" for="medicine_phone">
                     <div class="input__label-text">Номер телефона*</div>
-                    <input class="input__input" type="tel" id="medicine_phone" data-inputmask="'mask': '+79999999999', 'clearIncomplete': 'true'" maxlength="12" x-autocompletetype="phone-full" name="<?=$inp_name?>" value="" required="">
+                    <input class="input__input" type="tel" id="medicine_phone" data-inputmask="'mask': '+79999999999',
+                    'clearIncomplete': 'true'" maxlength="12" x-autocompletetype="phone-full" name="<?=$inp_name?>"
+                    value="" required="">
                 </label></div>
             <?php } ?>
         </div>
@@ -82,7 +87,8 @@ $ans_array = $arResult["arAnswers"];
             <div class="contact-form__form-message">
                 <div class="input"><label class="input__label" for="medicine_message">
                     <div class="input__label-text">Сообщение</div>
-                    <textarea class="input__input" type="text" id="medicine_message" name="<?=$inp_name?>" value=""></textarea>
+                    <textarea class="input__input" type="text" id="medicine_message" name="<?=$inp_name?>" value="">
+                    </textarea>
                     <div class="input__notification"></div>
                 </label></div>
             </div>
@@ -90,12 +96,12 @@ $ans_array = $arResult["arAnswers"];
         <?php } ?>
 
         <div class="contact-form__bottom">
-            <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что
-                ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных
-                данных&raquo;.
+            <div class="contact-form__bottom-policy">
+                Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что ознакомлены, полностью согласны и&nbsp;
+                принимаете условия &laquo;Согласия на&nbsp;обработку персональных данных&raquo;.
             </div>
-            <button type="submit" name="web_form_submit" class="form-button contact-form__bottom-button" data-success="Отправлено"
-                    data-error="Ошибка отправки">
+            <button type="submit" name="web_form_submit" class="form-button contact-form__bottom-button" 
+            data-success="Отправлено" data-error="Ошибка отправки">
                 <div class="form-button__title">Оставить заявку</div>
             </button>
         </div>
