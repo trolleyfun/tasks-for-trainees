d2421bfe42a0c03f689907add6da7092e519fbf5
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-10-03 11:51:24 +0500

    Add catalog.section.list template

---
+++ template/bitrix/catalog.section.list/my_template/template.php
@@ -0,0 +1,31 @@
+<?php 
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+?>
+
+<div id="barba-wrapper">
+    <div class="article-list">
+    <?php foreach ($arResult["SECTIONS"] as $arSection): ?>
+        <a class="article-item article-list__item" href="<?=$arSection["SECTION_PAGE_URL"]?>" data-anim="anim-3">
+
+        <?php if (is_array($arSection["PICTURE"])): ?>
+            <div class="article-item__background"><img src="<?=$arSection["PICTURE"]["SRC"]?>" alt=""/></div>
+        <?php endif; ?>
+
+            <div class="article-item__wrapper">
+                <div class="article-item__title"><?=$arSection["NAME"]?></div>
+
+                <?php if ($arSection["DESCRIPTION"]): ?>
+                    <div class="article-item__content"><?=$arSection["DESCRIPTION"]?></div>
+                <? endif; ?>
+            </div>	
+            <!-- /.article-item__wrapper -->		
+        </a>
+        <!-- /.article-item.article-list__item -->
+
+    <?php endforeach; ?>
+    </div>
+    <!-- /.article-list -->
+</div>
+<!-- /#barba-wrapper -->
