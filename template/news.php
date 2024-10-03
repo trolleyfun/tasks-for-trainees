e5b6e6983dd978184269ed8872545b2a216c0283
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-10-03 10:50:44 +0500

    Create index page

---
+++ template/news.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) {
+    die();
+}
+
+$APPLICATION->IncludeComponent(
+    "bitrix:catalog.section.list",
+    "my_template",
+    array(
+        "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+        "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+        "SECTION_ID" => $arResult["VARIABLES"]["SECTION_ID"],
+        "SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"]
+    ),
+    $component
+);
