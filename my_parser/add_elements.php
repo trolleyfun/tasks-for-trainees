ef0b15b2a7093103cd6399674724e3257d119898
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-10-10 19:53:46 +0500

    Put functions into class

--- my_parser/add_elements.php
+++ my_parser/add_elements.php
@@ -2,92 +2,38 @@
 
 $_SERVER['DOCUMENT_ROOT'] = "/opt/lampp/htdocs";
 require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
-require_once("includes/functions.php");
+
+use lib\IBlockElementLoader;
 
 \Bitrix\Main\Loader::includeModule('iblock');
 
 $iblock_id = 11;
 $filename = 'my_vacancy.csv';
-$iBlockElement = new CIBlockElement;
 
-$rsProperties = CIBlock::getProperties($iblock_id, array(), array());
-$arProperties = [];
-while ($property = $rsProperties->GetNext()) {
-    $key = trim(strtolower($property['CODE']));
-    $arProperties[$key]['code'] = $property['CODE'];
-    $arProperties[$key]['type'] = $property['PROPERTY_TYPE'];
-    $arProperties[$key]['multiple'] = $property['MULTIPLE'];
-    $arProperties[$key]['value'] = '';
-}
+$iBlockElement = new CIBlockElement;
+$elementLoader = new IBlockElementLoader;
+$elementLoader->initProperties($iblock_id);
 
 if (($handle = fopen($filename, 'r'))) {
     $data = fgetcsv($handle, separator: ',');
     if ($data = fgetcsv($handle, separator: ',')) {
-        $columnHeaders = [];
-        foreach ($data as $header) {
-            $columnHeaders[] = trim(strtolower($header));
-        }
+        $elementLoader->initHeaders($data);
+
         while ($data = fgetcsv($handle, separator: ',')) {
-            foreach ($arProperties as $property) {
-                $property['value'] = '';
-            }
-            $element_name = '';
-
-            foreach ($data as $i=>$value) {
-                if (isset($columnHeaders[$i])) {
-                    $key = $columnHeaders[$i];
-                    $value = str_replace(["\r\n", "\n", "\r"], ' ', $value);
-                    if ($key == 'name') {
-                        $element_name = trim($value);
-                    } elseif (isset($arProperties[$columnHeaders[$i]])) {
-                        if ($arProperties[$key]['multiple'] == 'Y') {
-                            $value = divideMultipleProperties($value, '•');
-                            if ($arProperties[$key]['type'] == 'L') {
-                                foreach ($value as $i=>&$item) {
-                                    $id = getListValueId($arProperties[$key]['code'], $item);
-                                    if ($id) {
-                                        $item = $id;
-                                    } else {
-                                        $item = '';
-                                    }
-                                }
-                            }
-                            unset($item);
-                        } elseif ($arProperties[$key]['type'] == 'L') {
-                            $id = getListValueId($arProperties[$key]['code'], $value);
-                            if ($id) {
-                                $value = $id;
-                            } else {
-                                $value = '';
-                            }
-                        } else {
-                            $value = trim($value);
-                        }
-                        $arProperties[$key]['value'] = $value;
-                    }
-                }
-            }
-
-            if (isset($arProperties['date'])) {
-                $arProperties['date']['value'] = date('d.m.Y');
-            }
-
-            $loadProperties = [];
-            foreach ($arProperties as $property) {
-                $loadProperties[$property['code']] = $property['value'];
-            }
+            $elementLoader->clearElementValues();
+            $elementLoader->setElementValues($data, '•');
+            $elementLoader->setValueByName('date', date('d.m.Y'));
 
             $loadIBlockElement = [
                 "MODIFIED_BY" => $USER->GetID(),
                 "IBLOCK_SECTION_ID" => false,
                 "IBLOCK_ID" => $iblock_id,
-                "PROPERTY_VALUES" => $loadProperties,
-                "NAME" => $element_name,
+                "PROPERTY_VALUES" => $elementLoader->createPropertiesArrayLoader(),
+                "NAME" => $elementLoader->elementName,
                 "ACTIVE" => 'Y'
             ];            
             $iBlockElement->Add($loadIBlockElement);
         }
     }
-
     fclose($handle);
 }
---
+++ my_parser/lib/IBlockElementLoader.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace lib;
+
+\Bitrix\Main\Loader::includeModule('iblock');
+
+class IBlockElementLoader
+{
+    public $arrProperties = [];
+    public $arrHeaders = [];
+    public $elementName = '';
+
+    public function initProperties($iblock_id)
+    {
+        $rsProperties = \CIBlock::getProperties($iblock_id, array(), array());
+        while ($property = $rsProperties->GetNext()) {
+            $key = trim(strtolower($property['CODE']));
+            $this->arrProperties[$key]['code'] = $property['CODE'];
+            $this->arrProperties[$key]['type'] = $property['PROPERTY_TYPE'];
+            $this->arrProperties[$key]['multiple'] = $property['MULTIPLE'];
+            $this->arrProperties[$key]['value'] = '';
+        }
+    }
+
+    public function initHeaders($headers)
+    {
+        foreach ($headers as $value) {
+            $this->arrHeaders[] = trim(strtolower($value));
+        }
+    }
+
+    public function clearElementValues()
+    {
+        foreach ($this->arrProperties as $property) {
+            $property['value'] = '';
+        }
+        $this->elementName = '';
+    }
+
+    public function setElementValues($elementValues, $valuesDivider)
+    {
+        foreach ($elementValues as $i=>$value) {
+            if (isset($this->arrHeaders[$i])) {
+                $key = $this->arrHeaders[$i];
+                $value = str_replace(["\r\n", "\n", "\r"], ' ', $value);
+                if ($key == 'name') {
+                    $this->elementName = trim($value);
+                } elseif (isset($this->arrProperties[$key])) {
+                    if ($this->arrProperties[$key]['multiple'] == 'Y') {
+                        $value = self::divideMultipleProperties($value, $valuesDivider);
+                        if ($this->arrProperties[$key]['type'] == 'L') {
+                            foreach ($value as $i=>&$item) {
+                                $id = self::getListValueId($this->arrProperties[$key]['code'], $item);
+                                if ($id) {
+                                    $item = $id;
+                                } else {
+                                    $item = '';
+                                }
+                            }
+                        }
+                        unset($item);
+                    } elseif ($this->arrProperties[$key]['type'] == 'L') {
+                        $id = self::getListValueId($this->arrProperties[$key]['code'], $value);
+                        if ($id) {
+                            $value = $id;
+                        } else {
+                            $value = '';
+                        }
+                    } else {
+                        $value = trim($value);
+                    }
+                    $this->arrProperties[$key]['value'] = $value;
+                }
+            }
+        }
+    }
+
+    public function setValueByName($name, $value)
+    {
+        if (isset($this->arrProperties[$name])) {
+            $this->arrProperties[$name]['value'] = $value;
+        }
+    }
+
+    public function createPropertiesArrayLoader()
+    {
+        $loadProperties = [];
+        foreach ($this->arrProperties as $property) {
+            $loadProperties[$property['code']] = $property['value'];
+        }
+        return $loadProperties;
+    }
+
+    public static function getListValueId($property_code, $list_value)
+    {
+        $rsListValue = \CIBlockProperty::GetPropertyEnum(
+            $property_code,
+            array(),
+            ['VALUE' => trim($list_value)]
+        );
+        if ($listItem = $rsListValue->GetNext()) {
+            return $listItem['ID'];
+        } else {
+            return false;
+        }
+    }
+
+    public static function divideMultipleProperties($str, $divider)
+    {
+        $str = explode($divider, $str);
+        foreach ($str as $i=>&$item) {
+            $item = trim($item);
+            if ($item == '') {
+                array_splice($str, $i, 1);
+            } 
+        }
+        unset($item);
+        return $str;
+    }
+}
