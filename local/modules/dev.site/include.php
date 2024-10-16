146053eb7dff07a254fae88579c8c33a06860fa2
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-10-16 23:34:11 +0500

    Correct include.php

--- local/modules/dev.site/include.php
+++ local/modules/dev.site/include.php
@@ -13,12 +13,13 @@ function dev_site_autoload($className)
 
     $sModuleCheck = strtolower($arParts[0] . '.' . $arParts[1]);
 
-    if ($sModuleCheck != $sModuleId)
+    if ($sModuleCheck != $sModuleId) {
         return;
+    }
 
     $arParts = array_splice($arParts, 2);
     if (!empty($arParts)) {
-        $fileName = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . strtolower(implode(DIRECTORY_SEPARATOR, $arParts)) . '.php';
+        $fileName = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $arParts) . '.php';
         if (file_exists($fileName)) {
             require_once $fileName;
         }
