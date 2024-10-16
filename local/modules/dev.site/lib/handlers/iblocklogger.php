d3b6c6f991a09be64d1c33fb78f6fe5dee2d7193
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-10-16 20:12:16 +0500

    Add function to create elements in logger iblock

--- local/modules/dev.site/lib/handlers/iblocklogger.php
+++ local/modules/dev.site/lib/handlers/iblocklogger.php
@@ -10,42 +10,62 @@ class IblockLogger
 {
     public static function OnAfterIBlockElementAddUpdateHandler(&$arFields)
     {
-        if (!$element_iblock = self::getIBlockById($arFields['IBLOCK_ID'])) {
+        if (!$arFields['RESULT']) {
+            return;
+        }
+        if (!$element_iblock = self::getIBlockFieldsById($arFields['IBLOCK_ID'])) {
             return;
         }
         if ($element_iblock['CODE'] == LOGGER_CODE) {
             return;
         }
-        if (!$logger_iblock = self::getIBlockByCode(LOGGER_CODE)) {
+        if (!$logger_iblock = self::getIBlockFieldsByCode(LOGGER_CODE)) {
             return;
         }
-        if (!$logger_section_id = self::createLoggerSection(
+        if (!$element_fields = self::getElementFieldsById($arFields['ID'])) {
+            return;
+        }
+        if (!$logger_section_id = self::getLoggerSectionId(
             $logger_iblock['ID'],
             $element_iblock['NAME'],
             $element_iblock['CODE']
             )) {
-                return;
-            }
+            return;
+        }
+
+        $element_object = new \CIBlockElement;
+        $element_object->Add([
+            'IBLOCK_ID' => $logger_iblock['ID'],
+            'IBLOCK_SECTION_ID' => $logger_section_id,
+            'NAME' => $arFields['ID'],
+            'DATE_ACTIVE_FROM' => $element_fields['TIMESTAMP_X']
+        ]);
     }
 
-    public static function getIBlockByCode($iblock_code)
+    public static function getIBlockFieldsByCode($iblock_code)
     {
         $rsIBlocks = \CIBlock::GetList(array(), ['=CODE' => $iblock_code]);
-        return $rsIBlocks->Fetch();
+        return $rsIBlocks->GetNext();
     }
 
-    public static function getIBlockById($iblock_id)
+    public static function getIBlockFieldsById($iblock_id)
     {
         $rsIBlocks = \CIBlock::GetList(array(), ['=ID' => $iblock_id]);
-        return $rsIBlocks->Fetch();
+        return $rsIBlocks->GetNext();
     }
 
-    public static function createLoggerSection($logger_id, $iblock_name, $iblock_code)
+    public static function getElementFieldsById($element_id)
+    {
+        $rsElements = \CIBlockElement::GetByID($element_id);
+        return $rsElements->GetNext();
+    }
+
+    public static function getLoggerSectionId($logger_id, $section_name, $section_code)
     {
         $rsLoggerSections = \CIBlockSection::GetList(array(), [
             '=IBLOCK_ID' => $logger_id,
-            '=NAME' => $iblock_name,
-            '=CODE' => $iblock_code
+            '=NAME' => $section_name,
+            '=CODE' => $section_code
         ], false, ['ID'], false);
         if ($logger_section_item = $rsLoggerSections->Fetch()) {
             return $logger_section_item['ID'];
@@ -54,8 +74,8 @@ class IblockLogger
             return $section_object->Add([
                 'IBLOCK_ID' => $logger_id,
                 'IBLOCK_SECTION_ID' => false,
-                'NAME' => $iblock_name,
-                'CODE' => $iblock_code
+                'NAME' => $section_name,
+                'CODE' => $section_code
             ]);
         }
     }
