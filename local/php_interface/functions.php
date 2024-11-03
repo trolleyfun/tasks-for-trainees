56b270660986390eed58c3c55d561e0e8281247b
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-11-03 10:46:33 +0500

    Delete not used files

--- local/php_interface/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function my_is_int($value)
-{
-    return strval($value) == strval(intval($value));
-}
--- local/php_interface/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
-    die();
-}
-
-require_once($_SERVER['DOCUMENT_ROOT']."/local/php_interface/functions.php");
-
