0c18457629e5c024828764128fe4bb4c01ef3c4e
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-11-07 16:02:21 +0500

    Add methods to display resources from disk

---
+++ Yandex/DiskManager.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Yandex;
+
+use Arhitector\Yandex\Disk;
+use Arhitector\Yandex\Disk\Resource\Closed;
+
+require_once('vendor/autoload.php');
+
+class DiskManager
+{
+    protected $disk;
+
+    protected $resource;
+
+    public function __construct($oauthToken, $resourcePath)
+    {
+        $this->disk = new Disk($oauthToken);
+        $this->resource = $this->disk->getResource($resourcePath);
+    }
+
+    public function getParentPath()
+    {
+        $arPath = explode('/', $this->resource->path);
+        while (!array_pop($arPath) && count($arPath) > 0) {
+            continue;
+        }
+        array_push($arPath, '');
+        return implode('/', $arPath);
+    }
+
+    public function displayItems()
+    {
+        $result = '';
+
+        if ($parentPath = $this->getParentPath()) {
+            $result .= self::getFolderHtml(urlencode($parentPath), '..');
+        }
+
+        foreach ($this->resource->get('items') as $item) {
+            if ($item->isDir()) {
+                $result .= self::getFolderHtml(urlencode($item->get('path')), $item->get('name'));
+            } elseif ($item->isFile()) {
+                $result .= self::getFileHtml(urlencode($item->get('path')), $item->get('name'));
+            }
+        }
+
+        return $result;
+    }
+
+    public static function getFolderHtml($path, $name)
+    {
+        $result = '
+        <a href="index.php?dir='.urlencode($path).'" class="resource-item" title="'.$name.'">
+            <input type="checkbox">
+            <img src="images/folder.svg" alt="">
+            <h1>'.$name.'</h1>
+        </a>';
+        return $result;
+    }
+
+    public static function getFileHtml($path, $name)
+    {
+        $result = '
+        <div class="resource-item" title="'.$name.'">
+            <input type="checkbox">
+            <img src="images/image.svg" alt="">
+            <h1>'.$name.'</h1>
+        </div>';
+        return $result;
+    }
+}
--- index.php
+++ index.php
@@ -1,20 +1,13 @@
 <?php
 
-use Arhitector\Yandex\Disk;
-
-
-
+use Yandex\DiskManager;
 
 require_once('vendor/autoload.php');
+require_once('Yandex/DiskManager.php');
 require_once('token.php');
 
-$disk = new Disk(OAUTH_TOKEN);
-
-$resource = $disk->getResource('/Test/Test1/Test2');
-
-
-$collection = $resource->get('items');
-
+$dirPath = $_GET['dir'] ?? 'disk:/';
+$manager = new DiskManager(OAUTH_TOKEN, urldecode($dirPath));
 ?>
 
 <!DOCTYPE html>
@@ -31,18 +24,10 @@ $collection = $resource->get('items');
     </nav>
     <div class="container">
         <section id="resources">
-            <a href="index.php?dir=Папка1" class="resource-item">
-                <input type="checkbox">
-                <img src="images/folder.svg" alt="">
-                <h1>Папка1</h1>
-            </a>
-            <a href="index.php?dir=Папка2" class="resource-item">
-                <input type="checkbox">
-                <img src="images/folder.svg" alt="">
-                <h1>Папка2</h1>
-            </a>
+        <?php
+        echo $manager->displayItems();
+        ?>
         </section>
     </div>
 </body>
 </html>
-
