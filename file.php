130609ca1be746f18047f7466bcb29da992cf9ab
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-11-11 09:29:11 +0500

    Modify html

--- file.php
+++ file.php
@@ -48,6 +48,17 @@ if (isset($_POST['update_file_button'])) {
         <section id="file">
             <h1>Тип файла: <?=htmlspecialchars($arFile['type'])?></h1>
             <form id="update-file" action="" method="post">
+            <?php if ($arFile['isText']): ?>
+                <div class="form-container">
+                    <input type="hidden" name="csrf_token" value="<?=$_SESSION['csrf_token']?>">
+                    <button type="submit" name="update_file_button"
+                        class="form-button">Сохранить</button>
+                    <a href="<?=htmlspecialchars($arFile['downloadLink'])?>"
+                        class="form-button">Скачать</a>
+                    <a href="index.php?path=<?=htmlspecialchars(urlencode($arFile['parentPath']))?>"
+                        class="form-button">Назад</a>
+                </div>
+            <?php endif; ?>
                 <div class="input-container">
                     <label for="file_name">Название:</label>
                     <input type="text" name="file_name" id="file_name" class="form-input"
@@ -64,7 +75,7 @@ if (isset($_POST['update_file_button'])) {
                     <button type="submit" name="update_file_button"
                         class="form-button">Сохранить</button>
                     <a href="<?=htmlspecialchars($arFile['downloadLink'])?>"
-                        class="form-button">Загрузить</a>
+                        class="form-button">Скачать</a>
                     <a href="index.php?path=<?=htmlspecialchars(urlencode($arFile['parentPath']))?>"
                         class="form-button">Назад</a>
                 </div>
