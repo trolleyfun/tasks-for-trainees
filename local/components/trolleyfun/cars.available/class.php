6d2517939be65e6b1216dbddbb77f5c15c1165b6
Anatolii Grishin <trolleyfun.ufa@gmail.com>
2024-11-13 16:56:33 +0500

    Fix onPrepareComponentParams method, add docblocks

--- local/components/trolleyfun/cars.available/class.php
+++ local/components/trolleyfun/cars.available/class.php
@@ -18,34 +18,110 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 /**
  * Компонент выводит список доступных для поездки служебных автомобилей.
  *
- * Компонент по GET-запросу, содержащему время начала и окончания поездки, формирует список доступных
+ * Компонент по GET-запросу, содержащему дату и время начала и окончания поездки, формирует список доступных
  * для поездки служебных автомобилей. Учитывается, какие классы автомобилей доступны текущему пользователю.
+ *
+ * Дата и время должны передаваться в формате "ГГГГ-ММ-ДД ЧЧ:ММ" (24-часовой формат).
  */
 class CarsAvailableComponent extends CBitrixComponent
 {
+    /**
+     * @var int $userId ID текущего пользователя
+     */
     protected $userId;
 
+    /**
+     * @var string $timeFrom Время начала поездки в формате "ГГГГ-ММ-ДД ЧЧ:ММ" (24-часовой формат)
+     */
     protected $timeFrom;
 
+    /**
+     * @var string $timeTo Время окончания поездки в формате "ГГГГ-ММ-ДД ЧЧ:ММ" (24-часовой формат)
+     */
     protected $timeTo;
 
+    /**
+     * Обрабатывает параметры компонента.
+     *
+     * Параметры компонента:
+     * + `CARS_IBLOCK_CODE` &ndash; Символьный код инфоблока, в котором хранится информация об автомобилях.
+     * Необязательный параметр. Значение по умолчанию "cars".
+     * + `ORDERS_IBLOCK_CODE` &ndash; Символьный код инфоблока, в котором хранится информация о поездках.
+     * Необязательный параметр. Значение по умолчанию "orders".
+     * + `JOBS_IBLOCK_CODE` &ndash; Символьный код инфоблока, в котором хранится информация о должностях сотрудников.
+     * Необязательный параметр. Значение по умолчанию "jobs".
+     * + `DRIVERS_IBLOCK_CODE` &ndash; Символьный код инфоблока, в котором хранится информация о водителях.
+     * Необязательный параметр. Значение по умолчанию "drivers".
+     * + `TIME_FROM_ALIAS` &ndash; Название GET-параметра, в котором передается дата и время начала поездки.
+     * + `TIME_TO_ALIAS` &ndash; Название GET-параметра, в котором передается дата и время окончания поездки.
+     *
+     * @param array $arParams Параметры компонента
+     * @return array Обработанные параметры компонента
+     */
     public function onPrepareComponentParams($arParams)
     {
-        $arParams['CARS_IBLOCK_CODE'] = trim(((string)$arParams['CARS_IBLOCK_CODE'] ?? 'cars'));
-        $arParams['ORDERS_IBLOCK_CODE'] = trim(((string)$arParams['ORDERS_IBLOCK_CODE'] ?? 'orders'));
-        $arParams['JOBS_IBLOCK_CODE'] = trim(((string)$arParams['JOBS_IBLOCK_CODE'] ?? 'jobs'));
-        $arParams['DRIVERS_IBLOCK_CODE'] = trim(((string)$arParams['DRIVERS_IBLOCK_CODE'] ?? 'drivers'));
-        $arParams['TIME_FROM_ALIAS'] = trim(((string)$arParams['TIME_FROM_ALIAS'] ?? 'from'));
-        $arParams['TIME_TO_ALIAS'] = trim(((string)$arParams['TIME_TO_ALIAS'] ?? 'to'));
+        $arParams['CARS_IBLOCK_CODE'] = trim((string)($arParams['CARS_IBLOCK_CODE'] ?? 'cars'));
+        $arParams['ORDERS_IBLOCK_CODE'] = trim((string)($arParams['ORDERS_IBLOCK_CODE'] ?? 'orders'));
+        $arParams['JOBS_IBLOCK_CODE'] = trim((string)($arParams['JOBS_IBLOCK_CODE'] ?? 'jobs'));
+        $arParams['DRIVERS_IBLOCK_CODE'] = trim((string)($arParams['DRIVERS_IBLOCK_CODE'] ?? 'drivers'));
+        $arParams['TIME_FROM_ALIAS'] = trim((string)($arParams['TIME_FROM_ALIAS'] ?? 'from'));
+        $arParams['TIME_TO_ALIAS'] = trim((string)($arParams['TIME_TO_ALIAS'] ?? 'to'));
 
         return $arParams;
     }
 
+    /**
+     * Подключает языковые файлы.
+     *
+     * @return void
+     */
     public function onIncludeComponentLang()
     {
         Loc::loadMessages(__FILE__);
     }
 
+    /**
+     * Формирует данные, которые необходимо вывести на страницу сайта.
+     *
+     * Обрабатывает данные из массива $arParams и формирует массив $arResult, который передаётся в шаблон компонента.
+     *
+     * Структура массива $arResult:
+     * ```
+     * Array
+     * (
+     *      [TIME_FROM] =>
+     *      [TIME_TO] =>
+     *      [CLASSES] => Array
+     *                   (
+     *                          [порядковый_номер] =>
+     *                   )
+     *      [CARS] => Array
+     *                (
+     *                      [ID_автомобиля] => Array
+     *                                (
+     *                                      [ID] =>
+     *                                      [MODEL] =>
+     *                                      [REG_ID] =>
+     *                                      [CLASS] =>
+     *                                      [DRIVER] => Array
+     *                                                  (
+     *                                                      [ID] =>
+     *                                                      [FIRST_NAME] =>
+     *                                                      [SECOND_NAME] =>
+     *                                                      [AGE] =>
+     *                                                  )
+     *                                )
+     *                )
+     * )
+     * ```
+     *
+     * + `TIME_FROM` &ndash; Время начала поездки в формате "ГГГГ-ММ-ДД ЧЧ:ММ" (24-часовой формат).
+     * + `TIME_TO` &ndash; Время окончания поездки в формате "ГГГГ-ММ-ДД ЧЧ:ММ" (24-часовой формат).
+     * + `CLASSES` &ndash; Доступные для текущего пользователя классы автомобилей.
+     * + `CARS` &ndash; Информация о доступных для поездки автомобилях и водителях этих автомобилей.
+     *
+     * @return void
+     */
     public function executeComponent()
     {
         try {
@@ -63,6 +139,13 @@ class CarsAvailableComponent extends CBitrixComponent
         }
     }
 
+    /**
+     * Подключает модули.
+     *
+     * @param string ...$modules
+     * @return void
+     * @throws Dev\Site\Exceptions\ComponentException если не удалось подключить модуль.
+     */
     protected function checkModules(...$modules)
     {
         foreach ($modules as $m) {
@@ -72,6 +155,11 @@ class CarsAvailableComponent extends CBitrixComponent
         }
     }
 
+    /**
+     * Инициализирует свойства класса компонента и проверяет корректность входных данных.
+     *
+     * @return void
+     */
     protected function initComponentProperties()
     {
         global $USER;
@@ -85,7 +173,10 @@ class CarsAvailableComponent extends CBitrixComponent
         $this->timeTo = urldecode($_GET[$this->arParams['TIME_TO_ALIAS']] ?? '');
         if (!$this->timeFrom || !$this->timeTo) {
             throw new ComponentException(Loc::getMessage('TIME_NOT_SET'));
-        } elseif (!self::validateDate($this->timeFrom) || !self::validateDate($this->timeTo)) {
+        } elseif (
+            !self::validateDate($this->timeFrom, 'Y-m-d H:i')
+            || !self::validateDate($this->timeTo, 'Y-m-d H:i')
+        ) {
             throw new ComponentException(Loc::getMessage('INVALID_TIME'));
         } elseif ($this->timeFrom > $this->timeTo) {
             throw new ComponentException(Loc::getMessage('TIME_FROM_LARGER_THAN_TIME_TO'));
@@ -101,6 +192,11 @@ class CarsAvailableComponent extends CBitrixComponent
         }
     }
 
+     /**
+     * Формирует массив $arResult.
+     *
+     * @return array
+     */
     protected function getResultArray()
     {
         $result = ['TIME_FROM' => $this->timeFrom, 'TIME_TO' => $this->timeTo];
@@ -116,6 +212,14 @@ class CarsAvailableComponent extends CBitrixComponent
         return $result;
     }
 
+    /**
+     * Формирует массив с доступными для поездки автомобилями.
+     *
+     * Если нет ни одного доступного для поездки автомобиля, возвращает пустой массив.
+     *
+     * @param string|string[] $availableClasses Доступные для пользователя классы автомобилей
+     * @return array
+     */
     protected function getCarsAvailable($availableClasses)
     {
         $carOrderedIds = $this->getCarsOrdered();
@@ -144,6 +248,14 @@ class CarsAvailableComponent extends CBitrixComponent
         return $cars;
     }
 
+    /**
+     * Возвращает массив с доступными для текущего пользователя классами автомобилей.
+     *
+     * Если у пользователя не указана должность или не задан ни один доступный класс автомобилей,
+     * возвращает пустой массив.
+     *
+     * @return array
+     */
     protected function getUserCarClasses()
     {
         $arUser = UserTable::getRow([
@@ -173,6 +285,13 @@ class CarsAvailableComponent extends CBitrixComponent
         return $userCarClasses;
     }
 
+    /**
+     * Формирует массив, содержащий ID автомобилей, которые заняты в указанное для поездки время.
+     *
+     * Если все автомобили свободны, возвращает пустой массив.
+     *
+     * @return array
+     */
     protected function getCarsOrdered()
     {
         $rsCars = CIBlockElement::GetList(
@@ -195,6 +314,14 @@ class CarsAvailableComponent extends CBitrixComponent
         return array_unique($carIds);
     }
 
+    /**
+     * Формирует массив с информацией о водителе автомобиля.
+     *
+     * Если водитель с указанным ID не найден, возвращает пустую строку.
+     *
+     * @param int $driverId ID водителя автомобиля
+     * @return array
+     */
     protected function getDriverById($driverId)
     {
         if (!$driverId) {
@@ -208,17 +335,29 @@ class CarsAvailableComponent extends CBitrixComponent
             ['ID', 'PROPERTY_FIRST_NAME', 'PROPERTY_SECOND_NAME', 'PROPERTY_AGE']
         )->Fetch();
 
-        $result = [];
-        if ($arDriver) {
-            $result['ID'] = $arDriver['ID'];
-            $result['FIRST_NAME'] = $arDriver['PROPERTY_FIRST_NAME_VALUE'];
-            $result['SECOND_NAME'] = $arDriver['PROPERTY_SECOND_NAME_VALUE'];
-            $result['AGE'] = $arDriver['PROPERTY_AGE_VALUE'];
+        if (!$arDriver) {
+            $result = '';
+        } else {
+            $result = [
+                'ID' => $arDriver['ID'],
+                'FIRST_NAME' => $arDriver['PROPERTY_FIRST_NAME_VALUE'],
+                'SECOND_NAME' => $arDriver['PROPERTY_SECOND_NAME_VALUE'],
+                'AGE' => $arDriver['PROPERTY_AGE_VALUE']
+            ];
         }
 
         return $result;
     }
 
+    /**
+     * Проверяет корректность даты в формате строки.
+     *
+     * Возвращает true, если дата корректна. Возвращает false, если дата не корректна.
+     *
+     * @param string $date Дата
+     * @param string $format Формат даты. Необязательный параметр. По умолчанию "Y-m-d H:i".
+     * @return bool
+     */
     public static function validateDate($date, $format = 'Y-m-d H:i')
     {
         $d = DateTime::createFromFormat($format, $date);
